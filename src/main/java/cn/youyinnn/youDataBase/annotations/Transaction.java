5e537a23a9a83929cda80efc78e8ac2d2f1d7094
youyinnn <454947049@qq.com>
2017-09-20 22:54:00 +0800

    创建Transaction注解

--- src/main/java/cn/youyinnn/youDataBase/annotations/Transaction.java
+++ src/main/java/cn/youyinnn/youDataBase/annotations/Transaction.java
@@ -1,4 +1,10 @@
 package cn.youyinnn.youDataBase.annotations;
 
+import java.lang.annotation.*;
+
+@Inherited
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Transaction {
+
 }
