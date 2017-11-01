5c000f21a3b45e84ddca7ffca8aa4df89c5c6050
youyinnn <454947049@qq.com>
2017-11-01 14:55:37 +0800

    补充Transaction注解的注释

--- src/main/java/cn/youyinnn/youdbutils/ioc/annotations/Transaction.java
+++ src/main/java/cn/youyinnn/youdbutils/ioc/annotations/Transaction.java
@@ -9,7 +9,14 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Transaction {
 
+    // TODO：事务的传播行为可选功能待实现
+    /**
+     * @return 返回事务的传播行为策略 默认一个线程有且仅有一个事务
+     */
     String transactionSpread() default ServiceIocBean.PROPAGATION_REQUIRED;
 
+    /**
+     * @return 返回布尔值表示是否允许无效的update操作 如果不允许 则出现该情况的时候回报错 默认允许
+     */
     boolean allowNoneffectiveUpdate() default true;
 }
