462044e2c4699800d7d25acbb757c8d3f82f9fb5
youyinnn <454947049@qq.com>
2017-11-03 10:33:07 +0800

    转移autowired： 之前自动装配是在生成代理类的时候进行的，这样会有不需要进行代理的Service类中的自动装配无效的问题 将自动装配的时机转移到YouServiceIocContainer中的getService方法里 这样无论是否需要代理的Service类都可以拥有自动装配功能

--- src/main/java/cn/youyinnn/youdbutils/ioc/YouServiceIocContainer.java
+++ src/main/java/cn/youyinnn/youdbutils/ioc/YouServiceIocContainer.java
@@ -1,11 +1,18 @@
 package cn.youyinnn.youdbutils.ioc;
 
+import cn.youyinnn.youdbutils.dao.YouDao;
+import cn.youyinnn.youdbutils.dao.YouDaoContainer;
 import cn.youyinnn.youdbutils.exceptions.AutowiredLimitedException;
+import cn.youyinnn.youdbutils.ioc.annotations.Autowired;
 import cn.youyinnn.youdbutils.ioc.annotations.Scope;
 import cn.youyinnn.youdbutils.ioc.annotations.Transaction;
+import cn.youyinnn.youdbutils.ioc.annotations.YouService;
 import cn.youyinnn.youdbutils.ioc.proxy.TransactionProxyGenerator;
+import cn.youyinnn.youdbutils.utils.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,7 +28,7 @@ public class YouServiceIocContainer {
 
     private YouServiceIocContainer() {}
 
-    static void addSingletonYouService(ServiceIocBean serviceBean) throws AutowiredLimitedException {
+    private static void addSingletonYouService(ServiceIocBean serviceBean) throws AutowiredLimitedException {
 
         String className = serviceBean.getClassName();
         Class serviceClass = serviceBean.getServiceClass();
@@ -38,7 +45,7 @@ public class YouServiceIocContainer {
         singletonServiceMap.put(className,serviceBean);
     }
 
-    static void addPrototypeYouService(ServiceIocBean serviceBean) {
+    private static void addPrototypeYouService(ServiceIocBean serviceBean) {
 
         String className = serviceBean.getClassName();
         prototypeServiceMap.put(className,serviceBean);
@@ -54,17 +61,17 @@ public class YouServiceIocContainer {
                 return null;
             } else {
                 if (hasTransactionAnnotation(serviceClass)) {
-                    return TransactionProxyGenerator.getProxyObject(serviceIocBean.getServiceClass());
+                    return autowired(TransactionProxyGenerator.getProxyObject(serviceIocBean.getServiceClass()));
                 } else {
                     try {
-                        return serviceClass.newInstance();
+                        return autowired(serviceClass.newInstance());
                     } catch (InstantiationException | IllegalAccessException e) {
                         e.printStackTrace();
                     }
                 }
             }
         } else {
-            return serviceIocBean.getSingleton();
+            return autowired(serviceIocBean.getSingleton());
         }
 
         return null;
@@ -101,7 +108,29 @@ public class YouServiceIocContainer {
         return singletonServiceMap.containsKey(youService.getName()) | prototypeServiceMap.containsKey(youService.getName());
     }
 
-    public static void setYouService(Class<?> aClass) {
+    private static Object autowired(Object youService) throws AutowiredLimitedException {
+        // Autowired实现 只接受YouDao和YouService的自动装配
+        ArrayList<Field> declaredFields = ReflectionUtils.getDeclaredFields(youService, Autowired.class);
+        for (Field declaredField : declaredFields) {
+            Class<?> type = declaredField.getType();
+            YouService youServiceAnnotation = type.getAnnotation(YouService.class);
+            // 装载YouDao和YouService
+            if (youServiceAnnotation == null) {
+                if ("YouDao".equals(type.getSuperclass().getSimpleName())) {
+                    YouDao autowiredDao = YouDaoContainer.getDao(type);
+                    ReflectionUtils.setFieldValue(youService,declaredField.getName(),autowiredDao);
+                } else {
+                    throw new AutowiredLimitedException("不支持的自动装配类型：["+type.getSimpleName()+" "+declaredField.getName()+"].");
+                }
+            } else {
+                Object autowiredDaoYouService = YouServiceIocContainer.getYouService(type);
+                ReflectionUtils.setFieldValue(youService,declaredField.getName(),autowiredDaoYouService);
+            }
+        }
+        return youService;
+    }
+
+    static void setYouService(Class<?> aClass) {
         Scope scope = aClass.getAnnotation(Scope.class);
 
         if (!YouServiceIocContainer.hasYouService(aClass)) {
@@ -115,7 +144,6 @@ public class YouServiceIocContainer {
             } else {
                 YouServiceIocContainer.addPrototypeYouService(new ServiceIocBean(aClass, ServiceIocBean.PROTOTYPE));
             }
-            System.out.println("set       "+ aClass);
         }
     }
 
--- src/main/java/cn/youyinnn/youdbutils/ioc/proxy/TransactionProxyGenerator.java
+++ src/main/java/cn/youyinnn/youdbutils/ioc/proxy/TransactionProxyGenerator.java
@@ -1,20 +1,11 @@
 package cn.youyinnn.youdbutils.ioc.proxy;
 
-import cn.youyinnn.youdbutils.dao.YouDao;
-import cn.youyinnn.youdbutils.dao.YouDaoContainer;
-import cn.youyinnn.youdbutils.exceptions.AutowiredLimitedException;
-import cn.youyinnn.youdbutils.ioc.YouServiceIocContainer;
-import cn.youyinnn.youdbutils.ioc.annotations.Autowired;
 import cn.youyinnn.youdbutils.ioc.annotations.Transaction;
-import cn.youyinnn.youdbutils.ioc.annotations.YouService;
-import cn.youyinnn.youdbutils.utils.ReflectionUtils;
 import net.sf.cglib.proxy.Callback;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.NoOp;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 
 /**
  * @description:
@@ -23,7 +14,7 @@ import java.util.ArrayList;
  */
 public class TransactionProxyGenerator {
 
-    public static Object getProxyObject(Class youServiceClass) throws AutowiredLimitedException {
+    public static Object getProxyObject(Class youServiceClass) {
 
         boolean isAll = false;
 
@@ -48,35 +39,7 @@ public class TransactionProxyGenerator {
             enhancer.setCallbackFilter(new TransactionMethodCallbackFilter());
         }
 
-        Object proxyObject = enhancer.create();
-
-        // Autowired实现 只接受YouDao和YouService的自动装配
-        ArrayList<Field> declaredFields = ReflectionUtils.getDeclaredFields(proxyObject, Autowired.class);
-        for (Field declaredField : declaredFields) {
-            Class<?> type = declaredField.getType();
-            YouService youServiceAnnotation = type.getAnnotation(YouService.class);
-            // 装载YouDao和YouService
-            if (youServiceAnnotation == null) {
-                if ("YouDao".equals(type.getSuperclass().getSimpleName())) {
-                    YouDao dao = YouDaoContainer.getDao(type);
-                    ReflectionUtils.setFieldValue(proxyObject,declaredField.getName(),dao);
-                } else {
-                    throw new AutowiredLimitedException("不支持的自动装配类型：["+type.getSimpleName()+" "+declaredField.getName()+"].");
-                }
-            } else {
-                Object youService = YouServiceIocContainer.getYouService(type);
-
-                // 为空则意味着该service中没有@Transaction 也则意味着不需要代理而直接使用原生
-                if (youService == null) {
-                    YouServiceIocContainer.setYouService(type);
-                }
-                youService = YouServiceIocContainer.getYouService(type);
-
-                ReflectionUtils.setFieldValue(proxyObject,declaredField.getName(),youService);
-            }
-        }
-
-        return proxyObject;
+        return enhancer.create();
 
     }
 
